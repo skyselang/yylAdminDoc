a3747f295060a9efeb99e8e0f6cf45273280deb3
skyselang <215817969@qq.com>
2021-06-02 11:59:20 +0800

    修改：logo
    Signed-off-by: skyselang <215817969@qq.com>

--- vuepress/.vuepress/config.js
+++ vuepress/.vuepress/config.js
@@ -103,32 +103,32 @@ module.exports = config({
     },
 
     pwa: {
-      favicon: "/favicon.ico",
+      favicon: "/yylAdminDoc/favicon.ico",
       cachePic: true,
       apple: {
-        icon: "/assets/icon/apple-icon-152.png",
+        icon: "/yylAdminDoc/assets/icon/apple-icon-152.png",
         statusBarColor: "black",
       },
       msTile: {
-        image: "/assets/icon/ms-icon-144.png",
+        image: "/yylAdminDoc/assets/icon/ms-icon-144.png",
         color: "#ffffff",
       },
       manifest: {
         icons: [
           {
-            src: "/assets/icon/chrome-mask-512.png",
+            src: "/yylAdminDoc/assets/icon/chrome-mask-512.png",
             sizes: "512x512",
             purpose: "maskable",
             type: "image/png",
           },
           {
-            src: "/assets/icon/chrome-mask-192.png",
+            src: "/yylAdminDoc/assets/icon/chrome-mask-192.png",
             sizes: "192x192",
             purpose: "maskable",
             type: "image/png",
           },
           {
-            src: "/assets/icon/chrome-512.png",
+            src: "/yylAdminDoc/assets/icon/chrome-512.png",
             sizes: "512x512",
             type: "image/png",
           },
@@ -145,13 +145,13 @@ module.exports = config({
             url: "/guide/",
             icons: [
               {
-                src: "/assets/icon/guide-maskable.png",
+                src: "/yylAdminDoc/assets/icon/guide-maskable.png",
                 sizes: "192x192",
                 purpose: "maskable",
                 type: "image/png",
               },
               {
-                src: "/assets/icon/guide-monochrome.png",
+                src: "/yylAdminDoc/assets/icon/guide-monochrome.png",
                 sizes: "192x192",
                 purpose: "monochrome",
                 type: "image/png",
